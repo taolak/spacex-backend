2a6d4c9981584dbf7dd38085db39ac878613143f
T.K <taolak@TKs-MacBook-Pro.local>
2019-10-16 19:43:11 -0500

    Added deployment connfig-5

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import callback from './express-callback'
 import {
   getLandSuccess,
   getReused,
   getWithReddit,
   postRefreshed
 } from './controllers'
-import makeCallback from './express-callback'
+
+dotenv.config()
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/', (_req, res) => {
+app.get('/', (_, req, res) => {
   res.send('Welcome to spacex')
+  next()
 });
 
-app.get('/land-success', makeCallback(getLandSuccess))
-app.get('/reused', makeCallback(getReused))
-app.get('/with-reddit', makeCallback(getWithReddit))
-app.post('/refresh', makeCallback(postRefreshed))
+app.get('/land-success', callback(getLandSuccess))
+app.get('/reused', callback(getReused))
+app.get('/with-reddit', callback(getWithReddit))
+app.post('/refresh', callback(postRefreshed))
 
 const port = process.env.PORT || '5000'
 app.listen(port, () => {
