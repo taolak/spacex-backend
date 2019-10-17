67da54b3268eb2fa54fe95b0b265262192e76879
T.K <taolak@TKs-MacBook-Pro.local>
2019-10-16 21:18:57 -0500

    Refractor express even more...

--- src/data-access/index.js
+++ src/data-access/index.js
@@ -3,16 +3,14 @@ import appConst from '../constants/app'
 import mongodb from 'mongodb'
 
 const MongoClient = mongodb.MongoClient
-const dbName = process.env.SPACEX_DB_NAME
-let client = null
 
 export async function makeDb () {
   try {
-    client = await MongoClient.connect(
+    const client = await MongoClient.connect(
       process.env.SPACEX_DB_URI, 
       { useUnifiedTopology: true, useNewUrlParser: true }
     )
-    return client.db(dbName)
+    return client.db(process.env.SPACEX_DB_NAME)
   } catch (error) {
     return {code: 400, message: "Unable to reach database."}
   }
