59b484e69791342eb1ade88ee0fcd8030d3ae582
T.K <taolak@TKs-MacBook-Pro.local>
2019-10-20 16:19:20 -0500

    Deploy dist with CORS

--- dist/index.js
+++ dist/index.js
@@ -21,6 +21,11 @@ _dotenv.default.config();
 
 const app = (0, _express.default)();
 app.use(_bodyParser.default.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.get('/', (_req, res) => {
   res.send('Welcome to spacex');
   next();
